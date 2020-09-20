50f1f607ccef9e47626c7a6284d710fcb7f7658e
ballyees <shouldertip@gmail.com>
2020-09-20 13:38:00 +0700

    add: listener, route

---
+++ api/v1/blueprintSQL.py
@@ -0,0 +1,42 @@
+from sanic import Blueprint
+from sanic.response import json, text
+from .SQLite import SqlApiV1Obj
+from sanic.log import logger
+import json as jsonobj
+
+bp_v1 = Blueprint('v1', url_prefix='/api', version="v1")
+
+@bp_v1.listener('after_server_stop')
+async def close_connection(app, loop):
+    logger.info('close server')
+    await SqlApiV1Obj.close()
+
+@bp_v1.route('/', methods=["GET"])
+async def api_v1_root(request):
+    logger.info('request server from path /')
+    return json({'hello': 'is my api'})
+
+@bp_v1.route('/user', methods=["POST"])
+async def userPost(request):
+    logger.info('request server from path /user POST method')
+    body_data_byte = request.body.decode('utf-8')
+    data = jsonobj.loads(body_data_byte)
+    return json({
+        "parsed": True,
+        "url": request.url,
+        "query_string": request.query_string,
+        "args": request.args,
+        "query_args": request.query_args,
+        "body_byte": body_data_byte,
+        "body_json": data,
+        "dict_key": list(data.keys()),
+        "endpoint": request.endpoint,
+        "testSQLData": SqlApiV1Obj.test(2)
+    })
+
+@bp_v1.route('/user', methods=["GET"])
+async def userGet(request):
+    logger.info('request server from path /user GET method')
+    return json({
+        "testSQLData": SqlApiV1Obj.test(2)
+    })
