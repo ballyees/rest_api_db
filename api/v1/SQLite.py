84111b93fdaeba25699998084e1c256ef412c7fc
ballyees <shouldertip@gmail.com>
2020-09-20 17:30:21 +0700

    add: insert user, change: get user

--- api/v1/SQLite.py
+++ api/v1/SQLite.py
@@ -1,5 +1,9 @@
 import sqlite3
 from os.path import join, dirname, abspath
+from .SQLCommand import SQLCommand
+from .loggingFile import Logger
+from .model import User
+
 class SqlApiV1():
     def __init__(self, DBName):
         self.__PathDB = join(dirname(abspath(__file__)), DBName)
@@ -7,9 +11,9 @@ class SqlApiV1():
         self.__DBName = DBName
         self.__cur = self.__connector.cursor()
 
-    def closeDB(self, saveData=True):
+    async def closeDB(self, saveData=True):
         if saveData:
-            self.__connector.commit()    
+            self.__connector.commit()
         self.__connector.close()
         
     def getConnector(self):
@@ -19,20 +23,21 @@ class SqlApiV1():
         self.closeDB(savePreviousDB)
         self.__connector = sqlite3.connect(DBName)
 
-    def test(self, LimitRow=None):
-        Limit = ''
-        if LimitRow is not None:
-            Limit = f'LIMIT {LimitRow}'
-        sql_cmd = f'''
-                    SELECT SOH.CustomerID, C.FirstName, C.MiddleName, C.LastName, SOH.TotalDue
-                    FROM SalesOrderHeader as SOH, Customer as C
-                    WHERE SOH.CustomerID=C.CustomerID
-                    {Limit}
-            '''
+    def getUser(self, username):
+        sql_cmd = SQLCommand.getUser(username)
+        print(sql_cmd)
         rows = self.__cur.execute(sql_cmd)
         col = []
         for c in self.__cur.description:
             col.append(c[0])
         return [dict(zip(col, row)) for row in rows.fetchall()]
-
-SqlApiV1Obj = SqlApiV1('lab2.db')
+    
+    def insertUser(self, data):
+        if self.getUser(data['username']):
+            return {'Sucess': False, 'code': 'have username'}
+        else:
+            user = User(data['username'], data['password'])
+            self.__cur.execute(SQLCommand.insertUser(user.getUserJson()))
+            self.__connector.commit()
+            return 1
+SqlApiV1Obj = SqlApiV1('classicmodels.sqlite')
