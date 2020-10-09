80288a1df685c30a6ed139562f2d3b62068697cd
ballyees <kittinan_prompao@elearning.cmu.ac.th>
2020-10-09 14:12:15 +0700

    start

--- startserver.sh
+++ startserver.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate sanicAPI
-python main.py
+echo "Start restful-api Store"
+python D:/Subject/Database/dbproject/rest_api_db/main.py
