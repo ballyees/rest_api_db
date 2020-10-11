55e2c98ad6ed7cc2693e50493ad72c18239322ac
ballyees <shouldertip@gmail.com>
2020-10-11 17:00:51 +0700

    add real path

--- startserver.sh
+++ startserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ~/anaconda3/etc/profile.d/conda.sh
+source C:/ProgramData/Anaconda3/etc/profile.d/conda.sh
 conda activate sanicAPI
 echo "Start restful-api Store"
 python D:/Subject/Database/dbproject/rest_api_db/main.py
--- startserverPC.sh
+++ startserverPC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ~/anaconda3/etc/profile.d/conda.sh
+source C:/ProgramData/Anaconda3/etc/profile.d/conda.sh
 conda activate sanicAPI
 echo "Start restful-api Store"
 python E:/Workspace/python/sanic/projectDB/rest_api_db/main.py
